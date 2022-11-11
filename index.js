f020ac51ad9fc70f6c958cc741be649a681d8db7
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-11-11 19:40:50 +0530

    trying to fix cors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express=require('express');
 const app=express();
 const cors=require('cors');
-app.use(cors());
+app.use(cors(
+    {
+        origin:'*'
+    }
+));
 const MongoClient = require('mongodb').MongoClient;
 const dotenv=require('dotenv');
 dotenv.config();
