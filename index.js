f26cda50f92915022accb3e43ea600120cf88e92
abirbhattacharya82 <abirbhattacharya82@gmail.com>
2022-11-11 10:19:17 +0530

    env variables

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
 const express=require('express');
 const app=express();
+import { MongoClient } from 'mongodb';
+
+const connection_url=`mongodb+srv://<username>:<password>@cluster0.a42g8hw.mongodb.net/?retryWrites=true&w=majority`
+
+app.get('/find-by-name',(req,res)=>{
+    var name=req.query.name;
+    const agg = [
+    {
+        '$search': {
+        'text': {
+            'query': 'Ronaldo', 
+            'path': 'short_name'
+        }
+        }
+    }, {
+        '$sort': {
+        'overall': -1
+        }
+    }, {
+        '$limit': 5
+    }
+    ];
+
+    MongoClient.connect(
+    '',
+    { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    const coll = client.db('').collection('');
+    const cursor = coll.aggregate(agg);
+    const result = await cursor.toArray();
+    await client.close();
+});
 
 const port=process.env.PORT || 8000;
+
+app.listen(port);
